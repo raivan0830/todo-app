ca17d3b7dee132d369ee77d1ab033b787c1d56be
Jiwon Kang <raivan0830@gmail.com>
2020-09-24 11:50:10 +0900

    10.3.3 delate element page283

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
 import TodoListItem from './TodoListItem';
 import './TodoList.scss';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, onRemove }) => {
+  const box = useRef(null);
+
+  const scrollToBottom = () => {
+    const { clientHeight, scrollHeight } = box.current;
+    box.current.scrollTop = scrollHeight - clientHeight;
+  };
+
+  useEffect(scrollToBottom, [todos]);
+
   return (
-    <div className="TodoList">
+    <div className="TodoList" ref={box}>
       {todos.map((todo) => (
-        <TodoListItem todo={todo} key={todo.id} />
+        <TodoListItem todo={todo} key={todo.id} onRemove={onRemove} />
       ))}
     </div>
   );
